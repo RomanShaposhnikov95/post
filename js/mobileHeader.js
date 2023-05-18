a586c9dd043ec9c9b7d6ca08d864fc3fb147c1d8
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-05-18 16:20:43 +0300

    change menu

--- js/mobileHeader.js
+++ js/mobileHeader.js
@@ -1,19 +1,44 @@
 (function () {
     const mediaQuery = window.matchMedia('(min-width: 1100px)')
     const head = document.querySelector('.head')
+    const logo = document.querySelector('.head-logo')
+    const list = document.querySelectorAll('.mobile-menu-dropdown-list');
+    const dropdown = document.querySelectorAll('.mobile-menu-dropdown');
 
     if (mediaQuery.matches) {
+        dropdown.forEach(el => {
+            const listInside = el.querySelector('.mobile-menu-dropdown-list');
+            const next = el.querySelector('.nextToSecondMenu');
+
+            if (!listInside) {
+                next.style.position = "static"
+                el.style.position = "static"
+            }
+        })
         window.addEventListener('scroll', function() {
             if (window.scrollY > 30) {
-                head.style.padding = '10px 20px 10px 30px'
+                // head.style.padding = '10px 20px 10px 30px'
+
+                list.forEach(el => {
+                    el.style.padding = '26px 0 0 0'
+                })
+                overlay.style.top = `75px`
+
+                logo.style.padding = '10px 30px 10px 0'
+
             } else if (window.scrollY < 10 ){
-                head.style.padding = '20px 20px 20px 30px'
+                // head.style.padding = '20px 20px 20px 30px'
+                logo.style.padding = '20px 30px 20px 0'
+
+                list.forEach(el => {
+                    el.style.padding = '36px 0 0 0'
+                })
+                overlay.style.top = `95px`
             }
         });
     }
 
 
-    const dropdown = document.querySelectorAll('.mobile-menu-dropdown');
     const firstMenu = document.querySelector('.mobile-menu')
     const openSvg = document.querySelector('.open-svg')
     const closeSvg = document.querySelector('.close-svg')
@@ -54,6 +79,11 @@
             const nextToThirdMenu = m.querySelector('.nextToThirdMenu')
             const backToSecondMenu = m.querySelector('.backToSecondMenu')
             const thirdMenu = m.querySelector('.mobile-menu-dropdown-list-inside')
+
+            if (!thirdMenu) {
+                nextToThirdMenu.style.position = "static"
+                return
+            }
 
             nextToThirdMenu.addEventListener('click', () => {
                 thirdMenu.style.left = '0'
