cb8d7188dd12709c7b756dc62133278196e7a0ec
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-05-26 16:13:14 +0300

    add new header

--- js/headers.js
+++ js/headers.js
@@ -1,50 +1,79 @@
+const mediaQuery = window.matchMedia('(max-width: 1300px)')
 const levelOneMenu = document.querySelectorAll(".levelOne-li")
 
+const header =  document.querySelector(".header")
 
-levelOneMenu.forEach(el => {
-    const levelOneBtn = el.querySelector(".levelOne-li-btn")
-    const levelTwoMenu = el.querySelector(".levelTwo")
-    const levelTwoLi = el.querySelectorAll(".levelTwo-li")
+const menuToggler = document.querySelector(".header-toggler")
 
-    if (!levelTwoMenu) return
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 30) {
+        header.classList.add("scroll")
+    } else if (window.scrollY < 10 ){
+        header.classList.remove("scroll")
+    }
+});
 
-    levelOneBtn.addEventListener("click", () => {
-        levelTwoMenu.style.right = "0"
+if (mediaQuery.matches) {
+    menuToggler.addEventListener("click", () => {
+        header.classList.toggle('header-show')
     })
 
-    const liWrap = document.createElement('li')
-    liWrap.classList.add("back-menu")
-    const backToFirst = document.createElement('button')
-    backToFirst.textContent = levelOneBtn.innerHTML
-    backToFirst.addEventListener('click', () => {
-        levelTwoMenu.style.right = "-100%"
-    })
-    liWrap.appendChild(backToFirst)
-    levelTwoMenu.insertBefore(liWrap, levelTwoMenu.firstChild)
+    levelOneMenu.forEach(el => {
+        const levelOneBtn = el.querySelector(".levelOne-li-btn")
+        const levelTwoMenu = el.querySelector(".levelTwo")
+        const levelTwoLi = el.querySelectorAll(".levelTwo-li")
 
-    levelOneBtn.classList.add("content-true")
+        if (!levelTwoMenu) return
 
-    levelTwoLi.forEach(two => {
-        const levelTwoBtn = two.querySelector(".levelTwo-li-btn")
-        const levelThreeMenu = two.querySelector(".levelThree")
-
-        if (!levelThreeMenu) return
-
-        levelTwoBtn.addEventListener("click", () => {
-            levelThreeMenu.style.right = "0"
+        levelOneBtn.addEventListener("click", () => {
+            levelTwoMenu.style.right = "0"
         })
 
-        levelTwoBtn.classList.add("content-true")
-
-        const liWrapTwo = document.createElement('li')
-        liWrapTwo.classList.add("back-menu")
-        const backToSecond = document.createElement('button')
-        backToSecond.textContent = levelTwoBtn.innerHTML
-        backToSecond.addEventListener('click', () => {
-            levelThreeMenu.style.right = "-100%"
+        const liWrap = document.createElement('li')
+        liWrap.classList.add("back-menu")
+        const backToFirst = document.createElement('button')
+        backToFirst.textContent = levelOneBtn.innerHTML
+        backToFirst.addEventListener('click', () => {
+            levelTwoMenu.style.right = "-100%"
         })
-        liWrapTwo.appendChild(backToSecond)
-        levelThreeMenu.insertBefore(liWrapTwo, levelThreeMenu.firstChild)
-    })
+        liWrap.appendChild(backToFirst)
+        levelTwoMenu.insertBefore(liWrap, levelTwoMenu.firstChild)
 
-})
+        levelOneBtn.classList.add("content-true")
+
+        levelTwoLi.forEach(two => {
+            const levelTwoBtn = two.querySelector(".levelTwo-li-btn")
+            const levelThreeMenu = two.querySelector(".levelThree")
+
+            if (!levelThreeMenu) return
+
+            levelTwoBtn.addEventListener("click", () => {
+                levelThreeMenu.style.right = "0"
+            })
+
+            levelTwoBtn.classList.add("content-true")
+
+            const liWrapTwo = document.createElement('li')
+            liWrapTwo.classList.add("back-menu")
+            const backToSecond = document.createElement('button')
+            backToSecond.textContent = levelTwoBtn.innerHTML
+            backToSecond.addEventListener('click', () => {
+                levelThreeMenu.style.right = "-100%"
+            })
+            liWrapTwo.appendChild(backToSecond)
+            levelThreeMenu.insertBefore(liWrapTwo, levelThreeMenu.firstChild)
+        })
+
+    })
+} else {
+    levelOneMenu.forEach(el => {
+        const secondMenu = el.querySelector('.levelTwo')
+        const btn = el.querySelector('.levelOne-li-btn')
+
+        if (secondMenu) {
+            btn.classList.add('content-true')
+        }
+    })
+}
+
+
