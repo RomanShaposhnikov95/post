0f70c60cf84c453875fc10f872c53959dcc0aa72
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-06-09 15:53:50 +0300

    add new fields

--- js/calculator.js
+++ js/calculator.js
@@ -3,6 +3,12 @@ const weight = document.querySelector(".weight");
 const totalWeight = document.querySelector(".total-weight");
 const coefficient = document.querySelector(".coefficient");
 let snapSlider = document.getElementById('snap');
+let nonStandardWrap = document.getElementById('non-standard');
+
+let nonStandardLength = document.getElementById('non-standard-length');
+let nonStandardWidth = document.getElementById('non-standard-width');
+let nonStandardWeight = document.getElementById('non-standard-weight');
+
 
 noUiSlider.create(snapSlider, {
     start: 0,
@@ -26,27 +32,85 @@ noUiSlider.create(snapSlider, {
 Array.from(radios).forEach((radio) => {
     const w = radio.dataset.weight;
     const c = radio.dataset.coefficient;
+    const t = radio.dataset.type;
+
+    let oldLength = 0;
+    let oldWidth = 0;
+    let oldWeight = 0;
 
     const updateValues = (sliderValue) => {
         const roundedValue = Math.round(sliderValue);
-        weight.textContent = `${roundedValue * w} kg`;
-        totalWeight.textContent = `${roundedValue * w * roundedValue} kg`;
-        coefficient.textContent = `${roundedValue * c}`;
+        weight.textContent = `${w} kg`;
+        totalWeight.textContent = `${w * roundedValue} kg`;
+        coefficient.textContent = `${(roundedValue * c).toFixed(2)}`;
+    };
+
+    const updateNoNonStandardValues = (nsLength, nsWidth, nsWeight, sliderValue) => {
+
+        if (nsLength !== null) oldLength = nsLength;
+
+        if (nsWidth !== null) oldWidth = nsWidth;
+
+        if (nsWeight !== null) oldWeight = nsWeight;
+
+        const roundedValue = Math.round(sliderValue);
+        weight.textContent = `${oldWeight} kg`;
+        totalWeight.textContent = `${oldWeight * roundedValue} kg`;
+        coefficient.textContent = `${(oldLength * oldWidth * roundedValue).toFixed(2)}`;
     };
 
     radio.addEventListener('change', () => {
         const sliderValue = snapSlider.noUiSlider.get();
-        console.log(sliderValue);
-        updateValues(sliderValue);
+
+        if (t === "false") {
+            nonStandardWrap.classList.remove('d-none')
+
+            weight.textContent = `0 kg`;
+            totalWeight.textContent = `0 kg`;
+            coefficient.textContent = `0`;
+
+            nonStandardLength.value = "0"
+            nonStandardWidth.value = "0"
+            nonStandardWeight.value = "0"
+
+        } else {
+            nonStandardWrap.classList.add('d-none')
+            updateValues(sliderValue);
+        }
     });
 
     snapSlider.noUiSlider.on('change', (values, handle) => {
         const newValue = values[handle];
         if (radio.checked) {
-            updateValues(newValue);
+           if (t === "false") {
+               updateNoNonStandardValues(null, null, null, newValue);
+           } else {
+               updateValues(newValue);
+           }
+
         }
     });
+
+    nonStandardLength.addEventListener("input", (e) => {
+        const sliderValue = snapSlider.noUiSlider.get();
+        const value = Number(e.target.value);
+        updateNoNonStandardValues(value, null, null, sliderValue);
+    })
+
+    nonStandardWidth.addEventListener("input", (e) => {
+        const sliderValue = snapSlider.noUiSlider.get();
+        const value = Number(e.target.value);
+        updateNoNonStandardValues(null, value, null, sliderValue);
+    })
+
+    nonStandardWeight.addEventListener("input", (e) => {
+        const sliderValue = snapSlider.noUiSlider.get();
+        const value = Number(e.target.value)
+        updateNoNonStandardValues(null, null, value, sliderValue);
+    })
 });
+
+
 
 
 
